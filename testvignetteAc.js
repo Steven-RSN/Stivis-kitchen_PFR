a2526adff4a343a5a4346b8dd16b96da07cc86e0
steven <stevenraison@outlook.fr>
2025-01-15 08:30:14 +0100

    Add number of person and comments

--- testvignetteAc.js
+++ testvignetteAc.js
@@ -1,48 +1,48 @@
 import recueil from "./glossaire.js";
 
 
-console.log(recueil); // Affichera toutes les recettes du glossaire
-console.log(recueil[0].titre); // Affichera "boeuf bourguignon"
-
+// Initialise les variables et récupère les élements HTML
 const titreR = document.getElementsByClassName('titreRecette');
 const titreRecetteTb = Array.from(titreR);
+const vignetteRecette = document.getElementsByClassName('img_recette');
 
-const vignietteRecette = document.getElementsByClassName('img_recette');
-const vignietteRecetteTb = Array.from(vignietteRecette);
+const vignetteRecetteTb = Array.from(vignetteRecette);  //convertit en tableau
+
+let compteurTemp=0
+let memory=[]
 
 
-for(let i =0 ; i<vignietteRecetteTb.length; i++){
-    titreRecetteTb[i].innerText=`${recueil[i].titre}`;
-    console.log(`${recueil[i].titre}`)
+// Boucle pour afficher les titres et les images des recettes situées dans glossaire.js
+for(let i =0 ; i<vignetteRecetteTb.length; i++){
 
-    vignietteRecetteTb[i].style.background=`url(${recueil[i].img})`;
-    vignietteRecetteTb[i].style.backgroundSize=`cover`;
+    titreRecetteTb[i].innerText=`${recueil[i].titre}`;                  
+    vignetteRecetteTb[i].style.background=`url(${recueil[i].img})`;
+    vignetteRecetteTb[i].style.backgroundSize=`cover`;
     
 }
 
-vignietteRecetteTb.forEach(function(recette,index) {
+// Applique un écouteur d'événements à chaque vignette 
+vignetteRecetteTb.forEach(function(recette,index) {
     
     recette.addEventListener('click',function(){
-        window.location.href = `recette.html?recette=${index}`;
+        window.location.href = `recette.html?recette=${index}`; // Renvoie vers la page recette correspondante
     })
     
 });
 
 
 
-const params = new URLSearchParams(window.location.search);
+const params = new URLSearchParams(window.location.search); // Récupère les paramètres de l'URL
 const recetteIndex = params.get('recette'); // Récupère l'index de la recette
 
-// si l'index existe et si c'est bien un nombre valide
+// Si l'index existe et si c'est bien un nombre valide, affiche la page recette correspondante
 if (recetteIndex !== null && !isNaN(recetteIndex)) {
 
-    const index = parseInt(recetteIndex)
+    const recette = recueil[recetteIndex]// Récupère la recette correspondante dans le tableau recueil
 
-    // Récupérer l'image de la recette en fonction de l'index
-    const recette = recueil[index]
 
+    // Initialise les variables pour récuperer des éléments HTML pour les modifier : titres et images (boucle pour les images)
     const titre=document.querySelector('h1')
-    console.log(titre)
     titre.innerText=recette.titre.charAt(0).toUpperCase() + recette.titre.slice(1)
 
  
@@ -51,56 +51,58 @@ if (recetteIndex !== null && !isNaN(recetteIndex)) {
 
     const imgPetiteRecette = document.querySelectorAll('.petiteImage')
     const imgPetiteTb = Array.from(imgPetiteRecette)
+
     for(let img of imgPetiteTb){
         img.src= recette.img;
       
     }
 
-    const h4=document.querySelector('.articleRecetteGrisIngrédiant h4')
-    h4.innerText=`Ingrédient pour ${recette.nbPersonne} personnes :`
+    const h4=document.querySelector('.nombre')
+    h4.innerText=`${recette.nbPersonne} `
 
+    // Initialise les variables pour récuperer des éléments HTML pour les modifier : ul et li.
+    // Chaque li (ingrédient de la liste) va être remplacé par les ingredients de la nouvelle recette , 
+    // On compare si la liste initiale et plus grande que la nouvelle (nouveaux ingredient), si elle est plus petite on rajoute des li
+    // sinon on les supprime 
     const ulIngre = document.querySelector('#ingrédientsUl')
     const listeIngredient = document.querySelectorAll('#ingrédientsUl li')
     const listeIngredientTb = Array.from(listeIngredient)
 
-    for(let i=0; i<recueil[index].ingredients.length;i++){
-       // console.log('taille de la liste ingré',recueil[index].ingredients.length)
-       // console.log('taille de la liste li',listeIngredientTb.length)
-        let result = recueil[index].ingredients.length - listeIngredientTb.length
-        console.log(result)
+    for(let i=0; i<recueil[recetteIndex].ingredients.length;i++){
+        let result = recueil[recetteIndex].ingredients.length - listeIngredientTb.length
+
         if(result>0){
             for(let j=0; j<result;j++){
+
                 const newLi= document.createElement('li')
                 ulIngre.appendChild(newLi)
                 listeIngredientTb.push(newLi)
-                console.log(listeIngredientTb)
             }
+            
         }else if(result<0){
             while(result<0){
+
                 const supLi= listeIngredientTb.pop()
                 supLi.remove()
                 result++
-            }
-           // console.log('ici',listeIngredientTb.splice(result))
-           // listeIngredientTb = listeIngredientTb.splice(result)
-           
-
+            }     
         }
 
         listeIngredientTb[i].innerText= `${recette.ingredients[i].quantité || ''} ${recette.ingredients[i].unité || ''} ${recette.ingredients[i].nom}`;
 
     }
 
+
+
+
+    // Même chose que pour la liste d'ingrédients mais pour les étapes
     const ulEtape = document.querySelector('#etapeUl');
    
     const listeEtape = document.querySelectorAll('#etapeUl li')
     const listeEtapeTb= Array.from(listeEtape)
 
-    for(let i=0; i<recueil[index].corps.length;i++){
-         console.log('taille de la liste corps',recueil[index].corps.length)
-         console.log('taille de la liste li',listeEtapeTb.length)
-         let result = recueil[index].corps.length - listeEtapeTb.length
-         console.log(result)
+    for(let i=0; i<recueil[recetteIndex].corps.length;i++){
+         let result = recueil[recetteIndex].corps.length - listeEtapeTb.length
         
         if(result>0){
 
@@ -108,7 +110,6 @@ if (recetteIndex !== null && !isNaN(recetteIndex)) {
                 const newLi= document.createElement('li')
                 ulEtape.appendChild(newLi)
                 listeEtapeTb.push(newLi)
-                console.log(listeIngredientTb)
             }
 
         }else if(result<0){
@@ -117,21 +118,92 @@ if (recetteIndex !== null && !isNaN(recetteIndex)) {
                 const supLi= listeEtapeTb.pop()
                 supLi.remove()
                 result++
-            }
-            // console.log('ici',listeIngredientTb.splice(result))
-            // listeIngredientTb = listeIngredientTb.splice(result)
-            
+            }         
  
         }
  
         listeEtapeTb[i].innerText= `${recette.corps[i]}`;
  
     }
-    
+
+
+
+
+   // Produit en croix afin de permettre d'incrémenter ou de décrémenter les quantités des ingrédients en fonction du nombre de personnes sélectionné
+    const btnIncrDecr= document.querySelectorAll('.decr, .incr')
+
+
+    const tableauBtn = Array.from(btnIncrDecr)
+    let nbPersonne=recette.nbPersonne
+    let newNbPersonne=recette.nbPersonne
+
+    tableauBtn.forEach(function(btn){
+        btn.addEventListener('click',function(){
+
+           
+            
+            // Incrémentation
+            if (btn.classList=='incr'){
+                console.log('click')
+                compteurTemp++
+                newNbPersonne=newNbPersonne+1
+                h4.innerText=`${newNbPersonne} `
+                console.log(compteurTemp, 'new', newNbPersonne)
+                for(let i=0; i<recueil[recetteIndex].ingredients.length;i++){
+
+                    let quantité=recueil[recetteIndex].ingredients[i].quantité
+
+                    let result=quantité*(newNbPersonne/nbPersonne)
+                    
+
+                    console.log('result :',result)
+                    
+                    memory.push(result)
+                    console.log(memory)
+                    listeIngredientTb[i].innerHTML=`${result||''} ${recette.ingredients[i].unité || ''} ${recette.ingredients[i].nom}`;
+
+             
+
+                }
+               
+            // Décrémentation
+            }else{
+                newNbPersonne=newNbPersonne-1
+                if(newNbPersonne===0){
+                    newNbPersonne=1
+                }
+                h4.innerText=`${newNbPersonne} `
+                console.log(compteurTemp, 'new', newNbPersonne)
+                for(let i=0; i<recueil[recetteIndex].ingredients.length;i++){          
+       
+
+                    let quantité=recueil[recetteIndex].ingredients[i].quantité
+
+                    let result=quantité*(newNbPersonne/nbPersonne)
+                    
+
+                    console.log('result :',result)
+                    
+                    memory.push(result)
+                    console.log(memory)
+                    listeIngredientTb[i].innerHTML=`${result||''} ${recette.ingredients[i].unité || ''} ${recette.ingredients[i].nom}`;
+                    
+                
+                }
+                
+                console.log('autre')
+
+            }
+
+            memory=[]
+
+        })
+    })
+
 
 
 } else {
-    console.error('Recette non trouvée.');
+    console.error('Recette non trouvée.');   //-TODO : Inplementer Try/Catch
 }
 
 
@@ -142,14 +214,32 @@ if (recetteIndex !== null && !isNaN(recetteIndex)) {
 
 
 
+/*
+
+//100g_farine /1_oignon/ 125cl_eau/ 60g_sucre
+let liste = [ 100 , 1 , 125 , 60 ]
+let nbPersonne=2
+
+
+let newNbPersonne = 4
+let newListe=[]
+
+    const listeIngredient=document.querySelectorAll('#ingrédientsUl li')
+
+
+
+    const tbIngredient=Array.from(listeIngredient)
+    console.log(tbIngredient)
+    for (let i=0; i<liste.length; i++){
+        newListe.push(liste[i]*(newNbPersonne/nbPersonne))    
+    }
+
+console.log(newListe)
 
 
 
 
-
-
-
-
+*/
 
 
 
@@ -197,4 +287,5 @@ Fournit des recettes détaillées avec des informations nutritionnelles et des l
 Lien : Edamam Recipe API
 d. Tasty API (non officiel)
 Certaines ressources non officielles permettent d'accéder aux recettes du site Tasty.
-Cherchez des projets open source sur GitHub ou via des API communautaires.*/
+Cherchez des projets open source sur GitHub ou via des API communautaires.*/          // console.log(recueil[recetteIndex].ingredients.length)
+                
